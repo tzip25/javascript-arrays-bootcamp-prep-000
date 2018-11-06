83e092cc75285527575baca31772ae0cef5f43be
Tali Scheer <tali.scheer@gmail.com>
2018-11-06 14:54:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,30 +1,55 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittle"];
 
-var arrayOne = [1];
 
 function addElementToBeginningOfArray() {
-var arrayNew = ['foo', ...arrayOne];
-return arrayNew;
+var arr1 = [1];
+var arr1New = ['foo', ...arr1];
+return arr1New;
 }
 
 function destructivelyAddElementToBeginningOfArray() {
-arrayOne.push('foo');
- return arrayOne; 
+var arr2 = [1];
+arr2.unshift('foo');
+return arr2;
 }
 
-var arrayTwo = [1];
-
 function addElementToEndOfArray() {
-var arrayNew = [...arrayTwo, 'foo'];
+var arr3 = [1];
+var arrayNew = [...arr3, 'foo'];
   return arrayNew;
 }
 
 function destructivelyAddElementToEndOfArray() {
-arrayTwo = [...arrayTwo, 'foo'];
-  return arrayTwo; 
+var arr4 = [1];
+arr4.push('foo');
+  return arr4; 
 }
 
 function accessElementInArray() {
-  var numberArray = [1, 2, 3,]
-  console.log(accessElementInArray[2]);
+  var arr5 = [1, 2, 3];
+  return(arr5[2]);
   }
+  
+function  destructivelyRemoveElementFromBeginningOfArray() {
+  var arr6 = [1, 2, 3];
+  arr6.shift();
+  return arr6;
+}
+
+function removeElementFromBeginningOfArray() {
+var arr7 = [1, 2, 3];
+arr7.slice(1);
+return arr7;
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+  var arr7 = [1, 2, 3];
+arr7.pop();
+return arr7;
+}
+
+function removeElementFromEndOfArray() {
+  var arr8 = [1, 2, 3];
+  arr8.slice(0, arr8.length - 1);
+  return arr8;
+}
